51d4fb2463b9266497f370760abe6c5d5fe9cb06
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-15 03:47:41 +0200

    1st trial

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s = 97, e = 122;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	s = 65;
+	e = 90;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
