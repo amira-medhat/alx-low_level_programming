e008ff34bca24351feb6e0f10412c4aa3e5e869b
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-15 15:37:52 +0200

    2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,15 @@
 
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i, j, k, q;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = 0; j <= 8; j++)
+		for (j = 0; j <= 8; j++)
 		{
-			for (int k = 0; k <= 9; k++)
+			for (k = 0; k <= 9; k++)
 			{
-				for (int q = 0; q <= 9; q++)
+				for (q = 0; q <= 9; q++)
 				{
 				putchar(i + '0');
 				putchar(j + '0');
