879a0489724ba7be7416b3ad22e1a98e17291bbd
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-21 02:08:06 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - converts the number to the abslute value
+ * @n: param represets the number.
+ * Return: n.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
