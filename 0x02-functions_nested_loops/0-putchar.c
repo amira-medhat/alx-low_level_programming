05c78bff0a0b4bed7e8375827122eeaf17d8bb19
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-20 23:38:50 +0300

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
