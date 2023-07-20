63b248038f1f897b847b871ae85f1363d3150eff
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-21 00:01:37 +0300

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+
+}
