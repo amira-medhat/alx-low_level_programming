f5cb5ea2a0820da66268801f113359b49404bfcc
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-22 00:25:13 +0300

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplyies 2 numbers
+ * @a: the first number.
+ * @b: the second number.
+ * Return: the result.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
