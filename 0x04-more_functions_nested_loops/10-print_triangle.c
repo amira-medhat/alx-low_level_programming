0a077775d3cc7c4c1bffb45159ee9e9318f15c29
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-22 04:41:46 +0300

    update task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@ void print_triangle(int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -20,6 +22,9 @@ void print_triangle(int size)
 			_putchar ('#');
 			j++;
 		}
+		_putchar ('\n');
 	}
+	}
+	else
 	_putchar ('\n');
 }
