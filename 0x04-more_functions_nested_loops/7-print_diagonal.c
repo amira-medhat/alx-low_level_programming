97fe5c9197a5b6df477b8fb606b0548fa0b03b95
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-22 03:24:51 +0300

    update task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,20 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i == n - 1)
-				_putchar ('\\');
-			else
+			for (j = 0; j < i; j++)
+			{
 				_putchar (' ');
+			}
+			_putchar ('\\');
+			_putchar ('\n');
 		}
 	}
+	else
 	_putchar ('\n');
 }
