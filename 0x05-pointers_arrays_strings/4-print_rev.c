3f867d2944b87440dabc303287466c074d28649e
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-24 12:12:15 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - to recognize the length of string
+ * @s: the string.
+ * Return: its length.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * print_rev - prints a string in a reverse order
+ * @s: the string.
+ */
+
+void print_rev(char *s)
+{
+	int strlength = _strlen(&s);
+	int i;
+
+	for (i = strlength - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
