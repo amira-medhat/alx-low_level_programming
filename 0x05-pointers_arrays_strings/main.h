f78fbeb402803416462da93791ecd0dea608cabb
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-24 11:20:59 +0300

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+int _putchar(char c);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+int print_digit(int n);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void printstr(char *str);
+
+void FizzBuzz(void);
+
+void print_triangle(int size);
+
+int isprime(long int n, int firsttime);
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
