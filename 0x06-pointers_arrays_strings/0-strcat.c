0b6f891fc48cdd93b8d2b7f4b530202a7d7161d8
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-07-28 21:08:09 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - to recognize the length of string
+ * @s: the string.
+ * Return: its length.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+
+/**
+ * _strcat - function appends a string to another
+ * @src: the first string.
+ * @dest: the second one.
+ * Return: the result string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int src_len = _strlen(src);
+	int i = src_len;
+	int k = 0;
+	int j = 0;
+	char *result[];
+
+	while (src[k] != '\0')
+	{
+		result[k] = src[k];
+		k++;
+	}
+
+	while (dest[j] != '\0')
+	{
+		result[src_len] = dest[j];
+		src_len++;
+		j++;
+	}
+	result[src_len] = '\0';
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,60 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+int mul(int a, int b);
+
+int _putchar(char c);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+int print_digit(int n);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void printstr(char *str);
+
+void FizzBuzz(void);
+
+void print_triangle(int size);
+
+int isprime(long int n, int firsttime);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _isdigit(char ch);
+
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
