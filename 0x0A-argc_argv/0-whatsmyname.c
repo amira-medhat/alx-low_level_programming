04da1bf51c4deae0b558212d4e1c363fdb18e8ce
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-05 20:09:57 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: arguments counter.
+ * @argv: array of arguments strings.
+ * Return: 1 on Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return EXIT_SUCCESS;
+}
