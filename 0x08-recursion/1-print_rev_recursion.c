c35f2f8fbe4a494cee94a27da56ed2ca4f3e7a41
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-06 03:05:01 +0300

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_rev_recursion - prints each char of string recursively
+ * @s: the string.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
