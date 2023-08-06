56d0bf4f9a722502238146ce00000e6e3cfde3fa
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-06 04:36:35 +0300

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates x power to y
+ * @x: the number.
+ * @y: the power.
+ * Return: the result.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
