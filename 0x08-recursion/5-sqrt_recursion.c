3c198bfa83c182183b1edebe9387163d5e15e679
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-06 04:55:34 +0300

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion_main - finds sqrt of number
+ * @num: the number.
+ * @val: used parm.
+ * Return: the result.
+ */
+
+int _sqrt_recursion_main(int num, int val)
+{
+	if (val * val > num)
+		return (-1);
+	if (val * val == num)
+		return (val);
+	_sqrt_recursion_main(num, val + 1);
+}
+
+/**
+ * _sqrt_recursion - task function
+ * @n: the number.
+ * Return: the result.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursion_main(n, 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,4 +16,8 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int _sqrt_recursion(int n);
+
+int _sqrt_recursion_main(int num, int val);
+
 #endif /* MAIN_H */
