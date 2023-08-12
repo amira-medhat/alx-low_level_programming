f69b1cd4c93c6ff71aaf7fc40d09e3f4c340ebf6
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-12 21:19:01 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * str_concat - function that concatenates two strings
@@ -7,44 +8,45 @@
  * @s2: the second string.
  * Return: pointer to the new string.
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int size;
+	int size, i = 0, j = 0;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
-		size = (int)(sizeof(char) * (strlen(s2) + 1));
-		ptr = (char *)malloc(size);
+		ptr = (char *)malloc(sizeof(char));
 		if (ptr == NULL)
-		{
 			return (NULL);
-		}
-		strcpy(ptr, s2);
+		ptr[0] = '\0';
 		return (ptr);
 	}
 
-	if (s2 == NULL)
-	{
-		size = (int)(sizeof(char) * (strlen(s1) + 1));
-		ptr = (char *)malloc(size);
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-		strcpy(ptr, s1);
-		return (ptr);
-	}
+	if (s1 != NULL)
+		size = strlen(s1) + 1;
+	if (s2 != NULL)
+		size = strlen(s2) + 1;
 
-	size = (int)(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	ptr = (char *)malloc(size);
 	if (ptr == NULL)
-	{
 		return (NULL);
+	if (s1 != NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			ptr[i] = s1[i];
+			i++;
+		}
 	}
-
-	strcpy(ptr, s1);
-	strcat(ptr, s2);
+	if (s2 != NULL)
+	{
+		while (s2[j] != '\0')
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
