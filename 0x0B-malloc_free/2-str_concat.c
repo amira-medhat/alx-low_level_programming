a39a330a16b0f0cb1158201dd24d285afef96424
amira <amira@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2023-08-12 20:47:52 +0300

    update task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,29 +12,32 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 	int size = (int)(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	int i;
+	int i = 0;
 	int j = 0;
 
-	if (s1 == NULL)
-		return (NULL);
 	ptr = (char *)malloc(size);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	i = 0;
-
-	while (s1[i] != '\0')
+	if (s1 != NULL)
 	{
-		ptr[i] = s1[i];
-		i++;
+		while (s1[i] != '\0')
+		{
+			ptr[i] = s1[i];
+			i++;
+		}
 	}
-	while (s2[j] != '\0')
+
+	if (s2 != NULL)
 	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
+		while (s2[j] != '\0')
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
 
 	ptr[i] = '\0';
