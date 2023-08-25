7235ce6eeb2a217118426a9f7557686009f1e887
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-25 21:55:59 +0300

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "lists.h"
+
+/**
+ * print_list - prints number of elemnts in linked list
+ * @h: pointer to the first node.
+ * Return: size of list.
+ */
+
+size_t print_list(const list_t *h)
+{
+  size_t i = 0;
+
+  while (h)
+    {
+      if (h -> str == NULL)
+	printf("[0] (nil)\n");
+      else
+	printf("[%ld] %s\n", strlen(h -> str), h -> str);
+      h = h -> next;
+      i++;
+    }
+  return (i);
+}
