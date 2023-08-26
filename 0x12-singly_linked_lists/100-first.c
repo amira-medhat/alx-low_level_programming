0a49900bc14d4f85a1e151d680a841a14125d35d
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-26 03:08:10 +0300

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _constructor - executes before main()
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
