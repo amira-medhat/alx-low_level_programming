3e4de28b84f7138519ad62a8aabadd5ff079ebaa
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-29 00:52:20 +0300

    final update of task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -37,6 +37,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		delete = current->next;
 		current->next = NULL;
+		free(delete);
 		return (1);
 	}
 	delete = current->next;
