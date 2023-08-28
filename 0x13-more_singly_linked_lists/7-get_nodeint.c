bb17df6828e9792cd53adb5d4385008ac49da8fe
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-28 21:03:55 +0300

    update task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = head;
 
+	if (head == NULL)
+		return (NULL);
 	while (i != index)
 	{
 		current = current->next;
 		i++;
+		if (current->next == NULL && i != index)
+			return (NULL);
 	}
 	return (current);
 }
