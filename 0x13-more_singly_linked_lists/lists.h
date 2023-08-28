26da7c3270b0bd1fa34138de806c1754396ee29e
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-28 18:08:30 +0300

    update task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,11 +7,11 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * @n: integer.
+ * @next: points to the next node.
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
