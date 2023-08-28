ad4c736b2585ba7bb9a3657163249fa476412dd3
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-28 17:23:44 +0300

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_all - prints different types of variables
+ * @format: the list of available variables to print.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	char *str = NULL;
+	char *empty = "(nil)";
+	int i = 0;
+	int flag = 0;
+
+	va_start(list, format);
+	while (format[i])
+	{
+	flag = 0;
+	switch (format[i])
+	{
+	case 'c':
+		printf("%c", va_arg(list, int));
+		break;
+	case 'i':
+		printf("%d", va_arg(list, int));
+		break;
+	case 'f':
+		printf("%f", va_arg(list, double));
+		break;
+	case 's':
+		str = va_arg(list, char *);
+		printf("%s", str ? str : empty);
+		break;
+	default:
+		flag = 1;
+		break;
+	}
+	if (!(format[i + 1] == '\0') && !flag)
+	printf(", ");
+	i++;
+	}
+	printf("\n");
+	va_end(list);
+}
