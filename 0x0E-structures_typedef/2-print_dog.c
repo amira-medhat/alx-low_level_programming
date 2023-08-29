087e11d94b776dd7b1127ec2f750dadbdcd20458
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-29 21:07:51 +0300

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints data of dog
+ * @d: pointer to the structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
