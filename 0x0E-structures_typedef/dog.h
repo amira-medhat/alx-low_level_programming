07e89352c6a080063339288691bf9347bab2359d
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-29 21:10:53 +0300

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  * @owner: its human owner.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
