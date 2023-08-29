4d02cdabb536eaac9c3cdb5625907230bf1bff60
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-29 22:40:24 +0300

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks if allocation has been done properly
+ * @b: the size of allocation.
+ * Return: pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (!ptr)
+	{
+		free(ptr);
+		exit(98);
+	}
+	return (ptr);
+}
