6609168c79e59177101ec08e10a52563451c8e83
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-30 18:46:47 +0300

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MAIN_H */
---
+++ 0x0D-preprocessor/4-sum.h~
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159265359
+
+#endif /* MAIN_H */
