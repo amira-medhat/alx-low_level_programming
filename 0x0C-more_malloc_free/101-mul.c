1e583e94b092b81039ab16fdb8a30068077e6642
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-30 05:57:01 +0300

    final

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,12 +49,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (!num1 || !num2)
-	{
-		printf("Error\n");
-		exit(98);
-	} 
+ 
 	num1 = argv[1];
 	num2 = argv[2];
 
