8be11f77c7f2fba7e3ba2dd42d013cec4c23aee9
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-09-03 23:09:57 +0300

    task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+
+unsigned int custom_pow(unsigned int base, unsigned int exponent);
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
