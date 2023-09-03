06c4292f860f51590cdc52e82bfca7001f497e4f
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-09-03 23:53:28 +0300

    update task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n > 1 && index > 0)
-	{
-		get_bit(n >> 1, index - 1);
-	}
-	if (index == 0)
-		return ((n & 1) ? '1' : '0');
-	return (-1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	return ((n >> index) & 1);
 }
