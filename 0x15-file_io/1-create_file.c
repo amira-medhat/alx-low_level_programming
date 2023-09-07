f4d2ae156eb4b79e38ec482c2ac02239ac213a27
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-09-08 00:46:05 +0300

    update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (!text_content)
+		while (*text_content != '\0')
 		{
+			text_content++;
 			i++;
 		}
 	}
