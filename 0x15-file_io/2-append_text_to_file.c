a85aa02fe3a8aa981770eefc3b655aff63996955
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-09-09 01:51:35 +0300

    update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,19 +13,16 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, _write = 0;
+	int fd;
+	ssize_t _write = 0;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-	{
-		close(fd);
-		return (-1);
-	}
-	if (strlen(text_content))
+
+	if (strlen(text_content) && text_content != NULL)
 	{
 		_write = write(fd, text_content, strlen(text_content));
 		if (_write == -1 || _write != (ssize_t)strlen(text_content))
