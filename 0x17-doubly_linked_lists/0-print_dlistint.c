59aa257f5f1255065ef40312e703c4c4fe5d2049
amira-medhat <amiramedhat2016@gmail.com>
2023-11-12 15:39:01 +0200

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,26 +7,22 @@
  *
  * Return: The number of nodes in the list.
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t * h)
 {
     /*Variable to store the number of nodes in the list.*/
     size_t nodes = 0;
-
 
     while (h)
     {
         /*Increment the node count.*/
         nodes++;
 
-
         /*Print the value of the current node.*/
-        printf("%d\n", h->n);
-
+        printf("%d\n", h -> n);
 
         /*Move to the next node.*/
-        h = h->next;
+        h = h -> next;
     }
-
 
     /*Return the total number of nodes in the list.*/
     return (nodes);
